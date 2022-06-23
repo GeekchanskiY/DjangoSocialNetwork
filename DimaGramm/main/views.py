71cea6a162a0484ae2f61bf67bc5199a15046587
GeekchanskiY <astrovskiy.04@gmail.com>
2022-06-23 10:28:51 +0300

    user register view added

--- DimaGramm/main/views.py
+++ DimaGramm/main/views.py
@@ -1,7 +1,19 @@
-from django.shortcuts import render
-from django.shortcuts import HttpResponse
+from django.shortcuts import render, HttpResponse
+from .forms import *
 # Create your views here.
 
 
 def index(request):
-    return HttpResponse("YAY")
+    return HttpResponse("Hi on DimaGramm")
+
+
+def user_register(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponse("User created successfully")
+        else:
+            return HttpResponse("Something went wrong")
+    else:
+        return render(request, 'index.html', {"form": CustomUserCreationForm})
