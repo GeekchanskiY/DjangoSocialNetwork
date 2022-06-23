72dd1c1ea51d1f9bc1a96460f4295e2e0389f2b5
GeekchanskiY <astrovskiy.04@gmail.com>
2022-06-23 10:05:50 +0300

    user admin created

--- DimaGramm/main/admin.py
+++ DimaGramm/main/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
-
+from .models import DMTUser
 # Register your models here.
+
+
+@admin.register(DMTUser)
+class DMTUserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'email')
+    fields = ('email', 'is_staff', 'is_active', 'date_joined', 'user_image', 'username')
+    search_fields = [
+        'username',
+        'email'
+    ]
