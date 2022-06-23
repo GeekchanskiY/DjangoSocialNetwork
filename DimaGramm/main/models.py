f36031480f8cbaebc93d8a6c1c8c0bf10983245f
GeekchanskiY <astrovskiy.04@gmail.com>
2022-06-23 10:05:39 +0300

    user model v1 created

--- DimaGramm/main/models.py
+++ DimaGramm/main/models.py
@@ -1,3 +1,26 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from django.utils import timezone
+from .managers import CustomUserManager
 # Create your models here.
+
+
+class DMTUser(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField('email address', unique=True)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    date_joined = models.DateTimeField(default=timezone.now)
+    user_image = models.ImageField(upload_to='uploads/', blank=True, null=True)
+    username = models.CharField(max_length=255, unique=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.email
+
+    class Meta:
+        verbose_name = "User"
+        verbose_name_plural = "Users"
